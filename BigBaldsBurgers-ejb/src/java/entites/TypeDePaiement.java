7053f958f4444a5a983a7f600e3c45918b89e506
Unknown <zerbib.jonathan@laposte.net>
2017-12-06 15:53:33 +0100

    maj entite (paiment/typePaiment)

--- BigBaldsBurgers-ejb/src/java/entites/TypeDePaiement.java
+++ BigBaldsBurgers-ejb/src/java/entites/TypeDePaiement.java
@@ -1,24 +1,31 @@
 package entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class TypeDePaiment implements Serializable {
+public class TypeDePaiement implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nom;
+    @ManyToMany(mappedBy = "typePaiements")
+    private Collection<Paiement> paiements;
 
-    public TypeDePaiment() {
+    public TypeDePaiement() {
+        paiements = new ArrayList<>();
     }
 
-    public TypeDePaiment(String nom) {
+    public TypeDePaiement(String nom) {
+        this();
         this.nom = nom;
     }
 
@@ -30,10 +37,20 @@ public class TypeDePaiment implements Serializable {
         this.nom = nom;
     }
 
+    public Collection<Paiement> getPaiments() {
+        return paiements;
+    }
+
+    public void setPaiments(Collection<Paiement> paiments) {
+        this.paiements = paiments;
+    }
+    
+
     public Long getId() {
         return id;
     }
 
+    
     public void setId(Long id) {
         this.id = id;
     }
