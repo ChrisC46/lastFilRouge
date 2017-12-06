f353f4e39848f4faafcb1d259927df7701c777c9
Unknown <william.kouamen@gmail.com>
2017-12-06 13:56:39 +0100

    entites will

---
+++ BigBaldsBurgers-ejb/src/java/entites/Commande.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entites;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author cdi312
+ */
+@Entity
+public class Commande implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Float prixTotal;
+    private Date date;
+
+    
+    public Commande() {
+    }
+
+    public Commande(Float prixTotal, Date date) {
+        this.prixTotal = prixTotal;
+        this.date = date;
+    }
+          
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getPrixTotal() {
+        return prixTotal;
+    }
+
+    public void setPrixTotal(Float prixTotal) {
+        this.prixTotal = prixTotal;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+  
+    @Override
+    public String toString() {
+        return "Date commande : " + date + ". " + " Prix Total : " + prixTotal;
+    }
+    
+}
---
+++ BigBaldsBurgers-ejb/src/java/entites/Employe.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entites;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author cdi312
+ */
+@Entity
+public class Employe implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int matricule;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String email;
+    private String login;
+
+  
+    public Employe() {
+    }
+
+    public Employe(int matricule, String nom, String prenom, String telephone, String email, String login) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.email = email;
+        this.login = login;
+    }
+   
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Employé matricule : " + matricule + "Nom, Prenom : " + nom + " " + prenom;
+    }
+    
+}
