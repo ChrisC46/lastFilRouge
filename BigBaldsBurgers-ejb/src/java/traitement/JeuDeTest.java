6dfe8b5a847d30b67cbeaaf882c74599be5beea9
Unknown <christophe.cuaz@gmail.com>
2017-12-08 09:59:50 +0100

    Maj jeu de test

--- BigBaldsBurgers-ejb/src/java/traitement/JeuDeTest.java
+++ BigBaldsBurgers-ejb/src/java/traitement/JeuDeTest.java
@@ -7,37 +7,44 @@ package traitement;
 
 import entites.FamilleIngredient;
 import entites.Ingredients;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Tofi
  */
-@Stateless
-public class JeuDeTestChris implements JeuDeTestChrisLocal {
-
-    @PersistenceContext(unitName = "BigBaldsBurgers-ejbPU")
- private EntityManager em;
- 
- @Override
- public void creerDonnees(){
- FamilleIngredient legumes = new FamilleIngredient("legumes");
+public class JeuDeTest {
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AeroportPU");
+        EntityManager em = emf.createEntityManager();
+        
+        FamilleIngredient legumes = new FamilleIngredient("legumes");
         FamilleIngredient viandes = new FamilleIngredient("viandes");
         FamilleIngredient pains = new FamilleIngredient("pains");
         FamilleIngredient liquides = new FamilleIngredient("liquides");
         FamilleIngredient fromages = new FamilleIngredient("fromages");
         FamilleIngredient sauces = new FamilleIngredient("sauces");
-       
+        
         Ingredients laitue = new Ingredients("laitue");
         Ingredients tomate = new Ingredients("tomate");
+        Ingredients frites = new Ingredients("frites");
+        Ingredients potatoes = new Ingredients("potatoes");
         Ingredients cornichon = new Ingredients("cornichon");
         Ingredients cheddar = new Ingredients("cheddar");
         Ingredients steackHC = new Ingredients("steackHC");
         Ingredients baldsSauce = new Ingredients("balds sauce");
         
+         em.persist(legumes);
+        em.persist(viandes);
+        em.persist(pains);
+        em.persist(liquides);
+        em.persist(fromages);
+        em.persist(sauces);
         
-       
- }
+        em.close();
+        emf.close();
+    }
 }
