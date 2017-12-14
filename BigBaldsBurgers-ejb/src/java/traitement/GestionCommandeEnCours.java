92892275c072e034a211925ae62688ac0184492d
Unknown <zerbib.jonathan@laposte.net>
2017-12-14 10:36:47 +0100

    maj / ajout de traitements

---
+++ BigBaldsBurgers-ejb/src/java/traitement/GestionCommandeEnCours.java
@@ -0,0 +1,11 @@
+
+package traitement;
+
+import javax.ejb.Singleton;
+
+
+@Singleton
+public class GestionCommandeEnCours implements GestionCommandeEnCoursLocal {
+
+    
+}
---
+++ BigBaldsBurgers-ejb/src/java/traitement/GestionEmplacement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traitement;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author cdi317
+ */
+@Stateless
+public class GestionEmplacement implements GestionEmplacementLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
