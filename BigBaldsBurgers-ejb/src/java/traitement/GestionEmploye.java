ba2750a2e751dafeabfed484ffb08a33cb9d5124
Unknown <christophe.cuaz@gmail.com>
2017-12-20 09:21:47 +0100

    maJ controle acces par login (controle)

--- BigBaldsBurgers-ejb/src/java/traitement/GestionEmploye.java
+++ BigBaldsBurgers-ejb/src/java/traitement/GestionEmploye.java
@@ -7,11 +7,13 @@ package traitement;
 
 import entites.Droits;
 import entites.Employe;
+import java.util.HashMap;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import outils.CustomedException;
 
 /**
  *
@@ -22,16 +24,23 @@ public class GestionEmploye implements GestionEmployeLocal {
     @PersistenceContext(unitName = "BigBaldsBurgers-ejbPU")
     private EntityManager em;
 
-    @Override
-    public List<Employe> findAllLoginByDroits(String droit){
-        Query qr = em.createNamedQuery("entites.Droits.getLoginParDroits");
-        qr.setParameter("paramDroits",droit );
-        List<Employe> le = qr.getResultList();
-        return le;
-    }
+//    @Override
+//    public List<Employe> findAllLoginByDroits(String droit){
+//        Query qr = em.createNamedQuery("entites.Droits.getLoginParDroits");
+//        qr.setParameter("paramDroits",droit );
+//        List<Employe> le = qr.getResultList();
+//        return le;
+//    }
     
     @Override
-    public List<Droits> findDroitsByLogin(String loginE){
+    public List<Droits> findDroitsByLogin(String loginE) throws CustomedException{
+        
+        HashMap<String, String> mp = new HashMap<>();
+        if(loginE == null || loginE.trim().isEmpty()){
+            mp.put("loginErr", "code obligatoire");
+        }else{
+            loginE = loginE.trim();
+        }
         Query qr = em.createNamedQuery("entites.Droits.getDroitsParLogin");
         qr.setParameter("paramLogin", loginE);
         List<Droits> ld = qr.getResultList();
@@ -48,13 +57,13 @@ public class GestionEmploye implements GestionEmployeLocal {
         return false;
     }
     
-    @Override
-    public Boolean isDroits (String loginE, String droit){
-          List<Employe> lEmp =  findAllLoginByDroits(droit);
-        for (Employe emp : lEmp){
-            if (emp.getLogin().equals(loginE))    
-               return true; 
-            }
-           return false; 
-    }
+//    @Override
+//    public Boolean isDroits (String loginE, String droit){
+//          List<Employe> lEmp =  findAllLoginByDroits(droit);
+//        for (Employe emp : lEmp){
+//            if (emp.getLogin().equals(loginE))    
+//               return true; 
+//            }
+//           return false; 
+//    }
 }
--- BigBaldsBurgers-ejb/src/java/traitement/GestionEmployeLocal.java
+++ BigBaldsBurgers-ejb/src/java/traitement/GestionEmployeLocal.java
@@ -9,6 +9,7 @@ import entites.Droits;
 import entites.Employe;
 import java.util.List;
 import javax.ejb.Local;
+import outils.CustomedException;
 
 /**
  *
@@ -16,12 +17,12 @@ import javax.ejb.Local;
  */
 @Local
 public interface GestionEmployeLocal {
+//
+//    public Boolean isDroits(String loginE, String droit);
+//
+//    public List<Employe> findAllLoginByDroits(String droit);
 
-    public Boolean isDroits(String loginE, String droit);
-
-    public List<Employe> findAllLoginByDroits(String droit);
-
-    public List<Droits> findDroitsByLogin(String loginE);
+    public List<Droits> findDroitsByLogin(String loginE) throws CustomedException;
 
     public Boolean isLogin(String loginE, String droit);
     
